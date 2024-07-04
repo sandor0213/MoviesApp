5e48c731cd059f69a6fd9bc26f7f47938eb2bd1b
Balogh Sandor <balogh.sandor0213@gmail.com>
2024-07-04 02:35:29 +0200

    Added save & fetch MoviesModelEntity to getMoviesPopular APIRequest

--- MoviesApp/Services/API/APIRequests.swift
+++ MoviesApp/Services/API/APIRequests.swift
@@ -10,12 +10,18 @@ import Alamofire
 
 final class APIRequests {
     static func getMoviesPopular(completion: @escaping (Result<MoviesModel, AFError>) -> Void) {
-        APIService.request(
-            apiPath: Constants.API.moviePopularPath,
-            method: .get,
-            parameters: [:],
-            responseType: MoviesModel.self,
-            completion: completion
-        )
+        APIService.request(apiPath: Constants.API.moviePopularPath, method: .get, parameters: [:], responseType: MoviesModel.self) { result in
+            switch result {
+            case .success(let moviesModel):
+                StorageManager.shared.saveMoviesModel(moviesModel: moviesModel)
+                completion(.success(moviesModel))
+            case .failure(let error):
+                if let moviesModel = StorageManager.shared.fetchMoviesModel() {
+                    completion(.success(moviesModel))
+                } else {
+                    completion(.failure(error))
+                }
+            }
+        }
     }
 }
